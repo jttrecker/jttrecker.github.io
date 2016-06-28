6e5f99ee51ab7bb1eaf6dc699e2756add8831eca
John Trecker <jttrecker@gmail.com>
2016-06-28 12:00:46 -0700

    implement new tweet event handler

--- init.js
+++ init.js
@@ -1,19 +1,25 @@
 $(document).ready(function(){
   var $tweets = $('ul');
+  var $tweet = $('<li class="tweets"></li>');
   // $twitts.html('');
 
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
-    var timestamp = moment().startOf('minute').fromNow();
     var $tweet = $('<li class="tweets"></li>');
-    var $user = $('<a href="#" class="user"></a>');
+    var timestamp = moment().startOf('second').fromNow();
+    var $user = $('<a href  ="#" class="user"></a>');
     
     $tweet.text('@' + tweet.user + ': ' + tweet.message + " ....... " + timestamp);
     $tweet.appendTo($tweets);
     index -= 1;
   }
 
+  $('.btn').on('click', function() {
+    var newTweet = $('.textbox').val();
+    $tweet.text('@visitor' + ': ' + newTweet + " ....... " + timestamp);
+    $tweet.prependTo($tweets);
+  })
 });
 
 
